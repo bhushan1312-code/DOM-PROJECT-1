6839a7a9d86361e9d5c78d8c758c491590035f91
Bhushan Avhad <bhushanavhad1312@gmail.com>
2025-08-08 09:48:25 +0530

    dom project - 1

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+
+const a = document.getElementsByClassName("container")
+
+const buttons = document.querySelectorAll(".bottom button");
+
+ function click(){
+     
+    buttons.setAttribute("button","https://www.technocrazed.com/wp-content/uploads/2015/12/Simple-wallpaper-52.jpg")
+ }
